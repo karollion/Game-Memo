95819c88aa6bb102aef8621a871b81b492d6e1af
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2024-02-26 20:40:55 +0100

    Add react-ruter-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 // import Components
 import Home from './components/pages/Home/Home';
+import Game from './components/pages/Game/Game';
+import Scores from './components/pages/Scores/Scores';
 
 function App() {
   return (
