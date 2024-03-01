c11959bc96b12eed892814b77b8454e20d73028d
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2024-03-01 21:13:33 +0100

    Add flip to card

--- client/src/components/features/GameCard/GameCard.tsx
+++ client/src/components/features/GameCard/GameCard.tsx
@@ -1,21 +1,32 @@
 import styles from './GameCard.module.scss';
-import React from 'react';
+import React, { useState } from 'react';
+import ReactCardFlip from 'react-card-flip';
 
 interface GameCardProps {
     children?: any;
 }
 
 const GameCard: React.FC<GameCardProps> = ({ children }) : JSX.Element => {
+  const [isFlipped, setIsFlipped] = useState(true);
 
-    const handleClick = () => {
-    }
+  const handleClick = ( e: any ) => {
+    e.preventDefault();
+    setIsFlipped(!isFlipped)
+  }
 
-    return (
-        <div 
-            className={styles.root}
-            onClick={handleClick}
-        ></div>
-    );
+  return (
+    <div className={styles.root} >
+      <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+        <div className={styles.front} onClick={handleClick}>
+          
+        </div>
+
+        <div className={styles.back} onClick={handleClick}>
+          
+        </div>
+      </ReactCardFlip>
+    </div>
+  );
 }
 
 export default GameCard;
