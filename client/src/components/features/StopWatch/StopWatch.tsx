e803d0bff17266d03014109551c99eaa8a22edde
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2024-03-07 21:02:31 +0100

    Add convertMsToTime function in utils

--- client/src/components/features/StopWatch/StopWatch.tsx
+++ client/src/components/features/StopWatch/StopWatch.tsx
@@ -1,13 +1,40 @@
 import styles from './StopWatch.module.scss';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import convertMsToTime from '../../../utils/convertMsToTime';
 
 interface StopWatchProps {
   action: ( time : string ) => void;
 }
 const StopWatch: React.FC<StopWatchProps> = ({ action }) : JSX.Element => {
+  const [time, setTime] = useState<number>(0);
+  const [timer, setTimer] = useState<any>(null);
+
+  const startTimer = () => {
+    if(timer == null){
+      setTimer(setInterval(() => {
+        setTime(time => time + 1);
+      }, 1));
+    }
+  };
+
+  const stopTimer = () => {
+    clearInterval(timer);
+    setTimer(null);
+  };
+  
+  const resetTimer = () => {
+    setTime(0);
+  };
+  
+  useEffect(() => {
+    return () => {
+      if(timer) clearInterval(timer);
+    };
+  }, []);
+
   return (
     <div className={styles.root}>
-      StopWatch
+      <span className={styles.timer}>{convertMsToTime(time)}</span>
     </div>
   );
 }
