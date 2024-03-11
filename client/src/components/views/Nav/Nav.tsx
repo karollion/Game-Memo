f6e7ae29a88d22997bd523e4505f17861429be2e
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2024-03-11 21:49:09 +0100

    Add styles to nav

--- client/src/components/views/Nav/Nav.tsx
+++ client/src/components/views/Nav/Nav.tsx
@@ -1,16 +1,29 @@
 import { NavLink } from 'react-router-dom';
 import styles from './Nav.module.scss';
-import React from 'react';
+import React, { useState } from 'react';
 
 const Nav: React.FC = () : JSX.Element => {
-    return (
-        <div className={styles.root}>
-            <NavLink  to="/">Home</NavLink>
-            <NavLink  to="/game">Game</NavLink>
-            <NavLink  to="/instruction">Instruction</NavLink>
-            <NavLink  to="/scores">Scores</NavLink>
-        </div>
-    );
+  const [toggleNav, setToggleNav] = useState<boolean>(false);
+
+  return (
+    <div className={styles.root}>
+      <nav className={styles.navigation}>
+        <button
+          className={styles.toggleBtn}
+          onClick={() => setToggleNav((prev) => !prev)}>
+          {toggleNav ? 'x' : '='}
+        </button>
+        {toggleNav ? 
+          <ul>
+            <NavLink onClick={() => setToggleNav((prev) => !prev)} className={({ isActive }) => isActive ? styles.linkActive : styles.link} to="/">Home</NavLink>
+            <NavLink onClick={() => setToggleNav((prev) => !prev)} className={({ isActive }) => isActive ? styles.linkActive : styles.link} to="/game">Game</NavLink>
+            <NavLink onClick={() => setToggleNav((prev) => !prev)} className={({ isActive }) => isActive ? styles.linkActive : styles.link} to="/instruction">Instruction</NavLink>
+            <NavLink onClick={() => setToggleNav((prev) => !prev)} className={({ isActive }) => isActive ? styles.linkActive : styles.link} to="/scores">Scores</NavLink>
+          </ul>
+        : null}
+      </nav>
+    </div>
+  );
 };
 
 export default Nav;
