7110affab2868d71ee362923d7b0bbc6db98d345
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2024-03-12 21:18:13 +0100

    Add fontawesome incons in navi

--- client/src/components/views/Nav/Nav.tsx
+++ client/src/components/views/Nav/Nav.tsx
@@ -1,6 +1,8 @@
 import { NavLink } from 'react-router-dom';
 import styles from './Nav.module.scss';
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faXmark, faVolumeHigh, faVolumeXmark } from '@fortawesome/free-solid-svg-icons';
 
 const Nav: React.FC = () : JSX.Element => {
   const [toggleNav, setToggleNav] = useState<boolean>(false);
@@ -11,13 +13,15 @@ const Nav: React.FC = () : JSX.Element => {
         <button
           className={styles.toggleBtn}
           onClick={() => setToggleNav((prev) => !prev)}>
-          {toggleNav ? 'x' : '='}
+          {toggleNav ? <FontAwesomeIcon  className={styles.icon} icon={faXmark} /> : <FontAwesomeIcon  className={styles.icon} icon={faBars} />}
         </button>
         {toggleNav ? 
           <ul>
             <NavLink onClick={() => setToggleNav((prev) => !prev)} className={({ isActive }) => isActive ? styles.linkActive : styles.link} to="/">Home</NavLink>
             <NavLink onClick={() => setToggleNav((prev) => !prev)} className={({ isActive }) => isActive ? styles.linkActive : styles.link} to="/scores">Scores</NavLink>
             <NavLink onClick={() => setToggleNav((prev) => !prev)} className={({ isActive }) => isActive ? styles.linkActive : styles.link} to="/instruction">Instruction</NavLink>
+            <FontAwesomeIcon  className={styles.icon} icon={faVolumeHigh} />
+            <FontAwesomeIcon  className={styles.icon} icon={faVolumeXmark} />
           </ul>
         : null}
       </nav>
