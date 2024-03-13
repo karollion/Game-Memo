a8fc0cdc158deeee0d61f5e9387e032e330d192e
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2024-03-13 22:03:39 +0100

    Changes in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,37 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ScoresModule } from './scores/scores.module';
+import { join } from 'path';
+import * as cors from 'cors';
+import configuration from './config/configuration';
+// import modules
+import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
+import { ScoresModule } from './scores/scores.module';
 
 @Module({
-  imports: [ScoresModule, UsersModule],
+  imports: [
+    ScoresModule, 
+    UsersModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../', 'client', 'build'),
+    }),
+    PrismaModule,
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(cors()).forRoutes({
+      path: 'http://localhost:3000',
+      method: RequestMethod.ALL,
+    });
+  }
+}
