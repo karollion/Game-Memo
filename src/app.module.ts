92edebb359f28de9186885824e1f87e696771fc1
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2024-03-13 21:44:14 +0100

    Add users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ScoresModule } from './scores/scores.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ScoresModule],
+  imports: [ScoresModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
